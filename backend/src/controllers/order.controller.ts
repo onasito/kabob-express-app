4487fbb6e3f74fe3d9eca39a4c1763ee0e7f6c32
onasito <oarrechavala0@gmail.com>
2026-08-30 17:42:47 -0700

    Finished createOrderItems function

--- backend/src/controllers/order.controller.ts
+++ backend/src/controllers/order.controller.ts
@@ -1,9 +1,11 @@
 import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma.js";
+import { Prisma } from "../generated/prisma/client.js";
 import { z } from "zod";
 
 const orderItemSchema = z.object({
-    quantity: z.number().int(),
-
+    menuItemId: z.number(),
+    quantity: z.number().int().positive(),
 })
 
 const orderSchema = z.object({
@@ -12,11 +14,39 @@ const orderSchema = z.object({
     items: z.array(orderItemSchema).min(1),
 })
 
+type resolvedOrderItem = {
+    menuItemId: number,
+    quantity: number,
+    price: Prisma.Decimal
+}
+
+export async function createOrderItem(menuItemId: number, quantity: number): Promise<resolvedOrderItem | null> {
+    const menuItem = await prisma.menuItem.findUnique({ where: { id: menuItemId }});
+    if (!menuItem) {
+        return null;
+    };
+
+    if (!menuItem.isAvailable) {
+        return null;
+    };
+
+    const data = {
+        menuItemId,
+        quantity,
+        price: menuItem.price
+    }
+
+    return data;
+
+}
+
 export async function createOrder(req: Request, res: Response): Promise<void> {
     const result = orderSchema.safeParse(req.body);
     if (!result.success) {
         res.status(400).json({ message: result.error.issues[0]?.message });
         return;
     };
+
+
 
 }
